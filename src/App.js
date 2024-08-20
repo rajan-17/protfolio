e64c5f279b24700b02e95d9b26925beffa651cb0
rajan-17 <kumar1234.rk16@gmail.com>
2024-08-20 17:02:00 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
 import './App.css';
-
-
 import Footer from './comp/Footer';
 import Header from './comp/Header';
 import AboutMe from './pages/AboutMe';
 import ContectUs from './pages/ContectUs';
-import Landing from './pages/Landing';
 import MySkill from './pages/MySkill';
 import Portfolio from './pages/Portfolio';
 
