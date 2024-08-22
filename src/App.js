6d7992ddd56be3dd76d6f18d9225d9746e5feb28
rajan-17 <kumar1234.rk16@gmail.com>
2024-08-22 12:18:11 +0530

    cursor update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ import AboutMe from './pages/AboutMe';
 import ContectUs from './pages/ContectUs';
 import MySkill from './pages/MySkill';
 import Portfolio from './pages/Portfolio';
-
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
   return (
     <>
+    <AnimatedCursor
+    
+    innerSize={8}
+    outerSize={50}
+    color='225, 225, 225'
+    outerAlpha={0.2}
+    innerScale={1}
+    outerScale={2}
+    clickables={[
+      'a',
+      'input[type="text"]',
+      'input[type="email"]',
+      'input[type="number"]',
+      'input[type="submit"]',
+      'input[type="image"]',
+      'label[for]',
+      'select',
+      'textarea',
+      'button',
+      '.link',
+    
+    ]}
+    />
       <Header />
       <AboutMe />
       <MySkill/>
