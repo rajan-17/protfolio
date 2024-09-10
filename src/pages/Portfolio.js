5cb35bbe3601e927092353c58a379a2a9080ffa7
rajan-17 <kumar1234.rk16@gmail.com>
2024-09-10 21:27:44 +0530

    new

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,70 @@
 import React from 'react'
-import project1 from '../img/UI-Screen-1.png'
+import project1 from '../img/1.jpeg'
+import project2 from '../img/2.jpeg'
+import project3 from '../img/3.jpeg'
+import project4 from '../img/4.jpeg'
+import project5 from '../img/5.png'
+
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+
 
 const Portfolio = () => {
   return (
-    <div className="about-color">
+<>
+<div className="container">
+
+<h4><span className='icon-color'>/</span>Projects</h4>
+
+</div>
 
 
+<OwlCarousel className='owl-theme my-5' loop margin={4} dots= {false} nav={false}  autoplay autoplaySpeed={3000}  responsive= {
+                {
+                    '1':{
+                        items: 1
+                    },
+                    '1025': {
+                        items: 3
+                    }
+                }
+                
+            } >
+                    <div className='item mx-2'>
+                     <div className="projects">
 
-    </div>
+                           <img src={project1} alt="" />
+                     </div>
 
+                        </div>
+                        
+                   
+                    <div className='item mx-2'>
+                    <div className="projects">
+                           <img src={project2} alt="" />
 
+                  
+                    </div>
+                    </div>
+                    <div className='item mx-2'>
+                    <div className="projects">
+                           <img src={project3} alt="" />
+
+            
+                    </div>
+                    </div>
+                    <div className='item mx-2'>
+                    <div className="projects">
+                           <img src={project4} alt="" />
+
+                       
+                  
+                    </div>
+                    </div>
+                </OwlCarousel>
+</>
   )
 }
 
