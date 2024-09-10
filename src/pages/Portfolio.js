ec788cd92e0944bb9bef15a623b6349bb216e527
rajan-17 <kumar1234.rk16@gmail.com>
2024-09-10 17:51:16 +0530

    not complete

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,44 +1,15 @@
 import React from 'react'
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'
-import 'swiper/css/effect-coverflow';
-import { Pagination,EffectCoverflow,Navigation } from 'swiper/modules';
 import project1 from '../img/UI-Screen-1.png'
 
 const Portfolio = () => {
   return (
     <div className="about-color">
 
-<Swiper
-      effect={'coverflow'}
-      grabCursor={true}
-      centeredSlides={true}
-      loop={true}
-      slidesPerView={3}
-      coverflowEffect={
-        {
-          rotate:0,
-          stretch:0,
-          depth:100,
-          modifier:2.5
-        }
-      }
-     Pagination={{el:'.swiper-pagination',clickable:true}}
-    >
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='project-img' src={[project1]} alt="" /></SwiperSlide>
 
-   ...
-    </Swiper>
+
     </div>
 
-    
+
   )
 }
 
