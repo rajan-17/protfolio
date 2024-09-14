167cd782a569eac1caf8866da2f0960004b5354b
rajan-17 <kumar1234.rk16@gmail.com>
2024-09-14 16:46:31 +0530

    90%

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,10 +4,10 @@ import project2 from '../img/2.jpeg'
 import project3 from '../img/3.png'
 import project4 from '../img/4.jpeg'
 import project5 from '../img/5.png'
-
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { Link } from 'react-router-dom'
 
 
 
@@ -21,7 +21,16 @@ const Portfolio = () => {
             </div>
 
 
-            <OwlCarousel className='owl-theme my-5' loop margin={4} dots={false} nav={false} autoplay autoplaySpeed={3000} responsive={
+            <OwlCarousel className='owl-theme my-5' 
+            loop 
+            margin={10}
+            dots={false}
+            nav={false}
+            autoplay
+            autoplaySpeed={3000}
+            autoplayHoverPause
+            autoWidth
+            responsive={
                 {
                     '1': {
                         items: 1
@@ -32,33 +41,50 @@ const Portfolio = () => {
                 }
 
             } >
-                <div className='item mx-2'>
+                <div className='item '>
                     <div className="projects">
 
                         <img src={project1} alt="" />
+                        <div className="projects-hover">
+                            <div className="visit">
+                              <Link to='https://github.com/rajan-17/instagram.git' target='_blank'>Visit</Link>  
+                                </div>
+                        </div>
                     </div>
 
                 </div>
 
 
-                <div className='item mx-2'>
+                <div className='item '>
                     <div className="projects">
                         <img src={project2} alt="" />
-
+                        <div className="projects-hover">
+                        <div className="visit">
+                        <Link to='https://protfolio-theta-eight.vercel.app/' target='_blank'>Visit</Link>
+                                </div>
+                        </div>
 
                     </div>
                 </div>
-                <div className='item mx-2'>
+                <div className='item '>
                     <div className="projects">
                         <img src={project3} alt="" />
-
+                        <div className="projects-hover">
+                        <div className="visit">
+                        <Link to='https://biooriginpharma.com/' target='_blank'>Visit</Link>
+                                </div>
+                        </div>
 
                     </div>
                 </div>
-                <div className='item mx-2'>
+                <div className='item'>
                     <div className="projects">
                         <img src={project4} alt="" />
-
+                        <div className="projects-hover">
+                        <div className="visit">
+                        <Link to='https://leafy-pika-1a74f8.netlify.app/' target='_blank'>Visit</Link>
+                                </div>
+                        </div>
 
 
                     </div>
